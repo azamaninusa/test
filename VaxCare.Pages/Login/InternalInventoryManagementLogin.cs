59e0d7c0f9cb2bd8d871772973bb6273c6204cd9
qabuffalo <92962310+qabuffalo@users.noreply.github.com>
2026-01-22 12:09:26 -0500

    test

---
+++ VaxCare.Pages/Login/InternalInventoryManagementLogin.cs
@@ -0,0 +1,34 @@
+using OpenQA.Selenium;
+using Serilog;
+using VaxCare.Core;
+using VaxCare.Core.Extensions;
+using VaxCare.Core.Logger;
+using VaxCare.Core.WebDriver;
+using VaxCare.Pages.InternalInventoryManagement;
+
+namespace VaxCare.Pages.Login
+{
+    public class InternalInventoryManagementLogin(IWebDriver driver, ILogger log) : BasePage(driver, log)
+    {
+        private static readonly string UserNameFieldId = "i0116";
+        private static readonly string NextButtonId = "idSIButton9";
+        private static readonly string PasswordFieldId = "input28";
+        private static readonly string OktaVerifyButton = "//input[@value='Verify']";
+        private static readonly string DoNotStaySignedIn = "idBtn_Back";
+        private static readonly string UserName = "qauser@vaxcare.com";
+        private static readonly string Password = "Samm1chesCheeseBac.n";
+
+        public async Task<InternalInventoryManagementPage> LoginAsync(string url)
+        {
+            Log.Step("Login to Internal Inventory Management.");
+            await Driver.NavigateAsync(url, Log);
+            await Driver.SendKeysAsync(UserNameFieldId.Id(), UserName, Log);
+            await Driver.ClickAsync(NextButtonId.Id(), Log);
+            await Driver.SendKeysAsync(PasswordFieldId.Id(), Password, Log, true);
+            await Driver.ClickAsync(OktaVerifyButton.XPath(), Log);
+            await Driver.ClickAsync(DoNotStaySignedIn.Id(), Log);
+            return await GoToAsync(async _ => new InternalInventoryManagementPage(Driver, Log));
+        }
+    }
+}
+
