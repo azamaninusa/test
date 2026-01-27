a9ebb733ceeef1ba1a36ed54f71ee73e78e02f6f
qabuffalo <92962310+qabuffalo@users.noreply.github.com>
2026-01-27 09:59:31 -0500

    base

---
+++ VaxCare.Core/BasePage.cs
@@ -0,0 +1,45 @@
+using Serilog;
+using VaxCare.Core.WebDriver;
+
+namespace VaxCare.Core
+{
+    public abstract class BasePage
+    {
+        protected IWebDriverActor Driver { get; }
+        protected ILogger Log { get; }
+
+        protected BasePage(IWebDriverActor driver, ILogger logger)
+        {
+            Driver = driver;
+            Log = logger;
+        }
+
+        /// <summary>
+        /// Allows for page initialization before the test begins
+        /// Override this if page requires preloading, waiting, login, or navigation
+        /// </summary>
+        /// <returns></returns>
+        public virtual Task InitAsync(params object[] args) => Task.CompletedTask;
+
+        /// <summary>
+        /// Fluent chaining.
+        /// </summary>
+        /// <returns>Initializes and returns the next page instance</returns>
+        protected async Task<T> GoToAsync<T>(T page, params object[] args) where T : BasePage
+        {
+            await page.InitAsync(args);
+            return page;
+        }
+
+        /// <summary>
+        /// Fluent chaining.
+        /// </summary>
+        /// <returns>Auto-constructs and initializes a new page</returns>
+        protected async Task<T> GoToAsync<T>(params object[] args) where T : BasePage
+        {
+            var page = (T)Activator.CreateInstance(typeof(T), Driver, Log)!;
+            await page.InitAsync(args);
+            return page;
+        }
+    }
+}
