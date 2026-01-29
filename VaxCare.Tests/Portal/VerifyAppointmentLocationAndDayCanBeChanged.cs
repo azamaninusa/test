f379a5370a807162afb1eb155ab84a337351eff3
qabuffalo <92962310+qabuffalo@users.noreply.github.com>
2026-01-29 17:54:24 -0500

    test

--- VaxCare.Tests/Portal/VerifyAppointmentLocationAndDayCanBeChanged.cs
+++ VaxCare.Tests/Portal/VerifyAppointmentLocationAndDayCanBeChanged.cs
@@ -1,6 +1,5 @@
 using Shouldly;
 using VaxCare.Core;
-using VaxCare.Core.Entities.Patients;
 using VaxCare.Core.Extensions;
 using VaxCare.Core.TestFixtures;
 using VaxCare.Pages.Login;
@@ -19,21 +18,22 @@ namespace VaxCare.Tests.Portal
         }
 
         [Theory]
-        [InlineData("https://identitystg.vaxcare.com", "QaRobot", "Verify Appointment Location and Day Can Be Changed")]
-        public async Task PortalVerifyAppointmentLocationAndDayCanBeChanged(string url, string userName, string testDescription)
+        [InlineData("https://identitystg.vaxcare.com", "QaRobot", "TammyRiskFree", "Verify Appointment Location and Day Can Be Changed")]
+        public async Task PortalVerifyAppointmentLocationAndDayCanBeChanged(string url, string userName, string testPatient, string testDescription)
         {
             var user = await _fixture.GetUserAsync(userName);
-            var patient = TestPatients.SharonRiskFree();
+            var patientRequest = await _fixture.GetPatientRequestAsync(testPatient);
+            var patient = await _fixture.GetTestPatientAsync(testPatient);
 
             await RunTestAsync(testDescription, async () =>
             {
-                await _fixture.SetupAsync(Log, user.ClinicId);
+                await _fixture.SetupAsync(Log, user.ClinicId)
+                    .Then(() => _fixture.AddTestPatientAppointment(patientRequest));
 
                 await PageAsync<PortalLogin>(user)
                     .Then(page => page.LoginAsync(url))
                     .Then(page => page.WaitForAppointmentGridToLoadAsync())
                     .Then(page => page.ChangeToDaysInAdvanceAsync(7))
-                    .Then(page => page.CheckInPatientAsync(patient))
                     .Then(page => page.ChangeLocationOnAppointmentAsync(patient, "QA Two"))
                     .Then(page => page.VerifyPatientIsNotListedAsync())
                     .Then(page => page.ChangeLocationOnPortalAsync("QA Two"))
@@ -42,7 +42,6 @@ namespace VaxCare.Tests.Portal
                     .Then(page => page.ChangeDateOnAppointmentAsync(1))
                     .Then(page => page.ChangeToDaysInAdvanceAsync(1))
                     .Then(page => page.VerifyPatientAppointmentExistsAsync(false))
-                    .Then(page => page.DeleteAppointmentAsync())
                     .Then(() => _fixture.TeardownAsync());
             });
         }
